d067fdc379050844cf0454301d64a1680c7e31c9
Atif Saddique <atif.saddique4@gmail.com>
2018-01-09 17:08:34 +0500

    add github scopes in config

--- src/main/java/io/curity/identityserver/plugin/github/authentication/GithubAuthenticatorRequestHandler.java
+++ src/main/java/io/curity/identityserver/plugin/github/authentication/GithubAuthenticatorRequestHandler.java
@@ -76,10 +76,21 @@ public class GithubAuthenticatorRequestHandler implements AuthenticatorRequestHa
         addQueryString(queryStringArguments, "state", state);
         addQueryString(queryStringArguments, "response_type", "code");
 
+        manageScopes(scopes);
+
+        addQueryString(queryStringArguments, "scope", String.join(" ", scopes));
+
+        _logger.debug("Redirecting to {} with query string arguments {}", AUTHORIZATION_ENDPOINT,
+                queryStringArguments);
+
+        throw _exceptionFactory.redirectException(AUTHORIZATION_ENDPOINT, MOVED_TEMPORARILY,
+                queryStringArguments, false);
+    }
+
+    private void manageScopes(Set<String> scopes) {
         _config.getManageOrganization().ifPresent(manageOrganization ->
         {
-            switch (manageOrganization.getAccess())
-            {
+            switch (manageOrganization.getAccess()) {
                 case WRITE:
                     scopes.add("write:org");
                     break;
@@ -90,14 +101,87 @@ public class GithubAuthenticatorRequestHandler implements AuthenticatorRequestHa
                     scopes.add("read:org");
             }
         });
-        
-        addQueryString(queryStringArguments, "scope", String.join(" ", scopes));
+        _config.getManageRepo().ifPresent(manageRepo -> {
+            if (manageRepo.isDeploymentStatusesAccess() && manageRepo.isPublicReposAccess()
+                    && manageRepo.isInviteAccess() && manageRepo.isReadWriteCommitStatus()) {
+                scopes.add("repo");
+            } else {
+                if (manageRepo.isDeploymentStatusesAccess()) {
+                    scopes.add("repo_deployment");
+                }
+                if (manageRepo.isInviteAccess()) {
+                    scopes.add("repo:invite");
+                }
+                if (manageRepo.isPublicReposAccess()) {
+                    scopes.add("public_repo");
+                }
+                if (manageRepo.isReadWriteCommitStatus()) {
+                    scopes.add("repo:status");
+                }
+            }
+        });
 
-        _logger.debug("Redirecting to {} with query string arguments {}", AUTHORIZATION_ENDPOINT,
-                queryStringArguments);
+        switch (_config.getPublicKeysAccess()) {
+            case WRITE:
+                scopes.add("write:public_key");
+                break;
+            case READ_WRITE:
+                scopes.add("admin:public_key");
+                break;
+            case READ:
+                scopes.add("read:public_key");
+        }
 
-        throw _exceptionFactory.redirectException(AUTHORIZATION_ENDPOINT, MOVED_TEMPORARILY,
-                queryStringArguments, false);
+        switch (_config.getRepoHooksAccess()) {
+            case WRITE:
+                scopes.add("write:repo_hook");
+                break;
+            case READ_WRITE:
+                scopes.add("admin:repo_hook");
+                break;
+            case READ:
+                scopes.add("read:repo_hook");
+        }
+
+        if (_config.isOrganizationHooks()) {
+            scopes.add("admin:org_hook");
+        }
+        if (_config.isGistsAccess()) {
+            scopes.add("gist");
+        }
+        if (_config.isNotificationsAccess()) {
+            scopes.add("notifications");
+        }
+
+        _config.getManageUser().ifPresent(manageUser -> {
+            if (manageUser.isEmailAccess() && manageUser.isFollowAccess()) {
+                scopes.add("user");
+            } else {
+                scopes.add("read:user");
+                if (manageUser.isEmailAccess()) {
+                    scopes.add("user:email");
+                }
+                if (manageUser.isFollowAccess()) {
+                    scopes.add("user:follow");
+                }
+            }
+        });
+
+        if (_config.isDeleteRepo()) {
+            scopes.add("delete_repo");
+        }
+
+        switch (_config.getGpgKeysAccess()) {
+            case WRITE:
+                scopes.add("write:gpg_key");
+                break;
+            case READ_WRITE:
+                scopes.add("admin:gpg_key");
+                break;
+            case READ:
+                scopes.add("read:gpg_key");
+        }
+
     }
 
     @Override
--- src/main/java/io/curity/identityserver/plugin/github/config/GithubAuthenticatorPluginConfig.java
+++ src/main/java/io/curity/identityserver/plugin/github/config/GithubAuthenticatorPluginConfig.java
@@ -17,11 +17,8 @@
 package io.curity.identityserver.plugin.github.config;
 
 import se.curity.identityserver.sdk.config.Configuration;
-import se.curity.identityserver.sdk.config.OneOf;
 import se.curity.identityserver.sdk.config.annotation.DefaultBoolean;
 import se.curity.identityserver.sdk.config.annotation.DefaultEnum;
-import se.curity.identityserver.sdk.config.annotation.DefaultString;
-import se.curity.identityserver.sdk.config.annotation.DefaultURI;
 import se.curity.identityserver.sdk.config.annotation.Description;
 import se.curity.identityserver.sdk.service.ExceptionFactory;
 import se.curity.identityserver.sdk.service.HttpClient;
@@ -30,7 +27,6 @@ import se.curity.identityserver.sdk.service.SessionManager;
 import se.curity.identityserver.sdk.service.WebServiceClientFactory;
 import se.curity.identityserver.sdk.service.authentication.AuthenticatorInformationProvider;
 
-import java.net.URI;
 import java.util.Optional;
 
 @SuppressWarnings("InterfaceNeverImplemented")
@@ -46,26 +42,86 @@ public interface GithubAuthenticatorPluginConfig extends Configuration {
 
     WebServiceClientFactory getWebServiceClientFactory();
 
-    interface ManageOrganization
-    {
+    enum Access {
+        NONE, WRITE, READ, READ_WRITE
+    }
+
+    interface ManageOrganization {
         @Description("Name of the organization to check that the user is a member of")
-        String getOrganizationName();
+        Optional<String> getOrganizationName();
 
         @Description("The level of access to the organization's data that the application requires")
-        Access getAccess();
-
         @DefaultEnum("READ")
-        enum Access
-        {
-            WRITE, READ, READ_WRITE;
-
-        }
+        Access getAccess();
     }
-    
+
+    @Description("Enable the application to manage repositories in GitHub")
+    Optional<ManageRepo> getManageRepo();
+
+    interface ManageRepo {
+        @Description("Request a scope (repo:status) that grants read/write access to public and private repository commit statuses. This scope is only necessary to grant other users or services access to private repository commit statuses without granting access to the code.")
+        @DefaultBoolean(true)
+        boolean isReadWriteCommitStatus();
+
+        @Description("Request a scope (repo_deployment) that grants access to deployment statuses for public and private repositories. This scope is only necessary to grant other users or services access to deployment statuses, without granting access to the code.")
+        @DefaultBoolean(true)
+        boolean isDeploymentStatusesAccess();
+
+        @Description("Request a scope (public_repo) that grants read/write access to code, commit statuses, collaborators, and deployment statuses for public repositories and organizations. Also required for starring public repositories.")
+        @DefaultBoolean(true)
+        boolean isPublicReposAccess();
+
+        @Description("Request a scope (repo:invite) that grants accept/decline abilities for invitations to collaborate on a repository. This scope is only necessary to grant other users or services access to invites without granting access to the code.")
+        @DefaultBoolean(true)
+        boolean isInviteAccess();
+    }
+
+    @Description("Enable the application to manage public keys in GitHub")
+    @DefaultEnum("NONE")
+    Access getPublicKeysAccess();
+
+    @Description("Enable the application to manage hooks in GitHub")
+    @DefaultEnum("NONE")
+    Access getRepoHooksAccess();
+
+    @Description("Request a scope (admin:org_hook) that grants read, write, ping, and delete access to organization hooks.")
+    @DefaultBoolean(false)
+    boolean isOrganizationHooks();
+
+    @Description("Request a scope (gist) that grants write access to gists.")
+    @DefaultBoolean(false)
+    boolean isGistsAccess();
+
+    @Description("Request a scope (notifications) that grants read access to a user's notifications.")
+    @DefaultBoolean(false)
+    boolean isNotificationsAccess();
+
+    @Description("Enable the application to manage user in GitHub")
+    Optional<ManageUser> getManageUser();
+
+    interface ManageUser {
+        @Description("Request a scope (user:email) that grants read access to a user's email addresses")
+        @DefaultBoolean(false)
+        boolean isEmailAccess();
+
+        @Description("Request a scope (user:follow) that grants access to follow or unfollow other users.")
+        @DefaultBoolean(false)
+        boolean isFollowAccess();
+    }
+
+
+    @Description("Request a scope (delete_repo) that grants access to delete adminable repositories.")
+    @DefaultBoolean(false)
+    boolean isDeleteRepo();
+
+    @Description("Enable the application to manage GPG keys in GitHub")
+    @DefaultEnum("NONE")
+    Access getGpgKeysAccess();
+
     // Services that don't require any configuration
 
     Json getJson();
-    
+
     SessionManager getSessionManager();
 
     Optional<HttpClient> getHttpClient();
